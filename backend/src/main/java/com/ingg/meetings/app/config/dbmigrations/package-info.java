2ceb9b12ce4697378987e5c324400cf96e8a2098
Rosa Abellan <rosa.abellan@ingg.com>
2018-11-07 11:38:10 +0000

    Starting backend project

---
+++ backend/src/main/java/com/ingg/meetings/app/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package com.ingg.meetings.app.config.dbmigrations;
---
+++ backend/src/main/java/com/ingg/meetings/app/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.ingg.meetings.app.domain;
