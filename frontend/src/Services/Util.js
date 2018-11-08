2f1f30d85c18809ddc802f635b403d54482407ed
Andrea Previtali <andrea.previtali@inseinc.com>
2018-11-08 13:19:19 +0000

    merge meeting array by id

--- frontend/src/Services/Util.js
+++ frontend/src/Services/Util.js
@@ -1,3 +1,3 @@
-export const merge = (events, newEvents) => {
-    return [...new Map(events.concat(newEvents).map((event)=>[event.id,event])).values()]
+const merge = (events, newEvents) => {
+    return [...new Map([...events,...newEvents].map((event)=>[event.id,event])).values()]
 }
